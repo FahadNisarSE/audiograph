e7456405166dd1cce3937d31e87591067f77af7d
AwaisRathore <49485532+AwaisRathore@users.noreply.github.com>
2024-05-08 22:14:33 +0500

    added timeline plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import WaveSurfer from "wavesurfer.js";
-import Regions from "wavesurfer.js/dist/plugins/regions.esm.js";
+import WaveSurfer, {WaveSurferOptions} from "wavesurfer.js";
+import TimelinePlugin from "wavesurfer.js/dist/plugins/timeline.esm.js";
 
 const timeContainer = document.getElementById("time");
 const playButton = document.getElementById("play");
@@ -10,22 +10,27 @@ const wavesurfer = WaveSurfer.create({
   waveColor: "#cdd3dc",
   progressColor: "#adb2b9",
   url: "/audio.wav",
+  plugins: [TimelinePlugin.create()],
 });
 
+wavesurfer.setOptions({minPxPerSec: 1000})
+
 wavesurfer.on("timeupdate", (time) => {
-   const minutes = Math.floor(time / 60);
-   const seconds = Math.floor(time % 60);
- 
-   const formattedTime = `${minutes < 10 ? '0' : ''}${minutes} : ${seconds < 10 ? '0' : ''}${seconds}`;
- 
-   if (timeContainer) timeContainer.innerText = formattedTime;
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  const formattedTime = `${minutes < 10 ? "0" : ""}${minutes} : ${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+
+  if (timeContainer) timeContainer.innerText = formattedTime;
 });
 
 wavesurfer.on("finish", () => {
   wavesurfer.pause();
   playButton?.classList.toggle("hide");
   pauseButton?.classList.toggle("hide");
-})
+});
 
 playButton?.addEventListener("click", () => {
   wavesurfer.play();
